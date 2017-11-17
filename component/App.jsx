a456743afdedd34b642604fd972dfe2d3921b3a9
Jeanne Castillo <jcaron27@gmail.com>
2017-11-17 16:47:58 -0500

    login with google

--- component/App.jsx
+++ component/App.jsx
@@ -1,23 +1,44 @@
-import React from 'react';
-import Test from './Test';
+import React, {Component} from 'react'
 import firebase, {auth} from '~/fire';
-import Images from './images'
 
-var provider = new firebase.auth.GoogleAuthProvider();
+import Login from './Login'
 
-function LoginWithGoogle() {
-    auth.signInWithPopup(provider);
-}
-
-auth.onAuthStateChanged(console.log);
-
-export default function App() {
-    return (
-    <div>
-        <Test />
-        <Images />
-        <button value="login" 
-        onClick={LoginWithGoogle}>Login</button>
+export const welcome = user => {
+    if (!user) return ''
+    if (user.isAnonymous) return ''
+    return 'Hello, ' + user.displayName || 'Hello, ' + user.email
+  }
+  
+  export const WhoAmI = ({user, auth}) =>
+    <div className="whoami">
+      { // If nobody is logged in, or the current user is anonymous,
+        (!user || user.isAnonymous)?
+        // ...then show signin links...
+        <Login />
+        /// ...otherwise, show a logout button.
+        : <button className='logout' onClick={() => auth.signOut()}>logout</button> }
     </div>
-        )
-}
+  
+  export default class App extends Component {
+    state = {};
+  
+    componentDidMount() {
+      this.unsubscribe = auth.onAuthStateChanged(user => this.setState({user}))
+    }
+  
+    componentWillUnmount() {
+      this.unsubscribe()
+    }
+  
+    render() {
+      const {user} = this.state || {}
+      console.log(this.state);
+      return (
+        <div>
+          <nav>
+            <WhoAmI user={user} auth={auth}/>
+          </nav>
+          <span className="whoami-user-name">{welcome(user)}</span>
+        </div>)
+    }
+  }
---
+++ component/Login.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import firebase, {auth} from '~/fire';
+
+const google = new firebase.auth.GoogleAuthProvider();
+// const facebook = new firebase.auth.FacebookAuthProvider();
+
+function Login(provider) {
+    auth.signInWithPopup(provider);
+}
+
+auth.onAuthStateChanged(console.log);
+
+export default ({ auth }) =>
+// signInWithPopup will try to open a login popup, and if it's blocked, it'll redirect.
+
+(<div className="login">
+    <button className='google login'
+    onClick={() => Login(google)}>Login with Google</button>
+</div>)
+
+
+// const email = new firebase.auth.EmailAuthProvider()
+
+// If you want to request additional permissions, you'd do it
+// like so:
+//
+// google.addScope('https://www.googleapis.com/auth/plus.login')
+//
+// What kind of permissions can you ask for? There's a lot:
+//   https://developers.google.com/identity/protocols/googlescopes
+//
+// For instance, this line will request the ability to read, send,
+// and generally manage a user's email:
+//
+// google.addScope('https://mail.google.com/')
