62538e79a62617785f741e9b2b0808e30b9abdda
Noorulain Khaliq <noorulainkhaliq@Noorulains-MacBook-Pro.local>
2017-11-18 17:59:57 -0500

    feature(user can add scene)

--- component/Routes.jsx
+++ component/Routes.jsx
@@ -7,6 +7,7 @@ import createMemoryHistory from 'history/createMemoryHistory';
 import Stories from './Stories';
 import AddStory from './AddStory';
 import SingleStory from './SingleStory';
+import AddScene from './AddScene';
 
 const history = process.env.NODE_ENV === 'test' ? createMemoryHistory() : createHistory();
 
@@ -16,6 +17,7 @@ const Routes = (props) => (
         <Route exact path='/stories' render={()=> <Stories currentUser={props.user} />} />
         <Route exact path='/stories/new' render={()=> <AddStory currentUser={props.user} />} />
         <Route exact path='/stories/:id' render={(newProps)=> <SingleStory currentUser={props.user} {...newProps} />} />
+        <Route exact path='/stories/:id/addscene' render={(newProps) =><AddScene currentUser={props.user} {...newProps}/>} />
     </Switch>
   </Router>
 )
