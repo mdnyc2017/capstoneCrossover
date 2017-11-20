652da991691048910963d9c655fbbfa4404b3db3
mcabrex <mrcabardo@gmail.com>
2017-11-20 12:37:18 -0500

    added scss skeleton

---
+++ component/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function Home() {
+  return (
+    <div>
+      <div>
+        <h1>Crossover</h1>
+      </div>
+      <div>2</div>
+      <div>3</div>
+      <div>4</div>
+    </div>
+  );
+}
