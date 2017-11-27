a6177eefb984994962fff7656aee24fc253212b4
Noorulain Khaliq <noorulainkhaliq@Noorulains-MacBook-Pro.local>
2017-11-27 10:24:22 -0500

    removed keys from addScene

--- component/AddScene.jsx
+++ component/AddScene.jsx
@@ -1,27 +1,27 @@
-import React, { Component } from 'react';
-import Dropzone from 'react-dropzone';
-import sha1 from 'sha1';
-import superagent from 'superagent';
-import { db } from '../fire';
-import { Redirect } from 'react-router';
-import Canvas from './Canvas';
+import React, { Component } from "react";
+import Dropzone from "react-dropzone";
+
+import superagent from "superagent";
+import { db } from "../fire";
+import { Redirect } from "react-router";
+import Canvas from "./Canvas";
 
 export default class AddScene extends Component {
   constructor(props) {
     super();
     this.state = {
-      storyId: '',
-      imageUrl: '',
-      previewUrl: '',
+      storyId: "",
+      imageUrl: "",
+      previewUrl: "",
       lineStrength: 20,
       colorReduction: 50,
-      id: '',
+      id: "",
       user: {},
       canvasImages: []
     };
     this.handleSubmitPreview = this.handleSubmitPreview.bind(this);
     this.handleChange = this.handleChange.bind(this);
-    this.handleAddOverlay = this.handleAddOverlay.bind(this);    
+    this.handleAddOverlay = this.handleAddOverlay.bind(this);
   }
 
   componentWillReceiveProps(nextProps) {
@@ -32,67 +32,20 @@ export default class AddScene extends Component {
   }
 
   uploadFile(files) {
-    //Plural files
-    const image = files[0]; //Assuming we are uploading one file at a time;
+    const image = files[0];
 
-    //Normally, an upload str is requested for authentication, but cloudinary deviates from the standard of doing so.
+    const url = "http://localhost:5001/crossover-cf663/us-central1/helloWorld"; //cloud func location
 
-    //cloudName, url, timestamp, uploadPreset will all be packaged into the upload request
-    const cloudName = 'noorulain'; //uniquely identifies account
-
-    //Constant string for uploading resource type can be many. image/pdf/video
-    const url =
-      'https://api.cloudinary.com/v1_1/' + cloudName + '/image/upload';
-
-    //Requires a timestamp in seconds, so it's necessary to divide by 1000
-    const timestamp = Date.now() / 1000;
-
-    //Found in settings as well
-    const uploadPreset = 'pvfhdtk2';
-
-    //Next create a parameter string
-    //Everything is in key:value pairs - it is a url param
-    const paramStr =
-      'timestamp=' +
-      timestamp +
-      '&upload_preset=' +
-      uploadPreset +
-      'ZoD3Vr3GEPRLq3dZdZCaiJbuwCY';
-
-    //Next encrypt using sha1 encryption --sha1 is a function
-    //This converts paramStr to sha1 signature --encrypted str is what goes to cloudinary
-    //It is their form of security
-    const signature = sha1(paramStr);
-
-    //Prepare JSON with params
-    const params = {
-      api_key: '493184569883823',
-      timestamp: timestamp,
-      upload_preset: 'pvfhdtk2',
-      signature: signature
-    };
-
-    //********************READY TO UPLOAD FILE USING ABOVE INFORMATION **********************/
-    //Superagent documentation:
-    //https://visionmedia.github.io/superagent/
-
-    let uploadRequest = superagent.post(url); //Request made
-    //Cloudinary requirement to name the file 'file'
-    //Key has to be file and the value is an image
-    uploadRequest.attach('file', image); // Upload file, attach is specific to superagent
-
-    //For every upload request, we are sending the key:value from params; loop created in the event we decide to allow users to upload multiple images.
-    Object.keys(params).forEach(key => {
-      uploadRequest.field(key, params[key]); //Rield is specific to superagent
-    });
+    let uploadRequest = superagent.post(url);
+    uploadRequest.attach("file", image);
 
     uploadRequest.end((err, resp) => {
-      //'end' sends the request
+      //'end' sends the request to the firebase function which is the backend -- located in index.js
       if (err) {
         return;
       }
-      const uploadUrl = resp.body.url;
-      const ind = uploadUrl.indexOf('upload/');
+      const uploadUrl = JSON.parse(resp.body.text).url;
+      const ind = uploadUrl.indexOf("upload/");
       const newUrl = `${uploadUrl.slice(0, ind + 7)}w_500/e_cartoonify:${
         this.state.lineStrength
       }:${this.state.colorReduction}${uploadUrl.slice(ind + 7)}`;
@@ -117,16 +70,16 @@ export default class AddScene extends Component {
   handleAddOverlay(image) {
     let self = this;
     setTimeout(() => {
-        self.setState({
-            canvasImages: [...self.state.canvasImages, image]
-        })
+      self.setState({
+        canvasImages: [...self.state.canvasImages, image]
+      });
     }, 10);
   }
 
   handleChange(evt) {
     // evt.preventDefault();
     const url = this.state.imageUrl;
-    const ind = url.indexOf('upload/');
+    const ind = url.indexOf("upload/");
     this.setState({
       [evt.target.name]: evt.target.value,
       previewUrl: `${url.slice(0, ind + 7)}w_500/e_cartoonify:${
@@ -180,18 +133,66 @@ export default class AddScene extends Component {
           storyId={this.state.storyId}
         />
         <div className="overlay-options">
-            <img src="/captionbox.png" onClick={() => this.handleAddOverlay('/captionbox.png')} width="100px" />
-            <img src="/quote1.png" onClick={() => this.handleAddOverlay('/quote1.png')} width="100px" />
-            <img src="/quote2.png" onClick={() => this.handleAddOverlay('/quote2.png')} width="100px" />
-            <img src="/quote3.png" onClick={() => this.handleAddOverlay('/quote3.png')} width="100px" />
-            <img src="/quote4.png" onClick={() => this.handleAddOverlay('/quote4.png')} width="100px" />
-            <img src="/quote5.png" onClick={() => this.handleAddOverlay('/quote5.png')} width="100px" />
-            <img src="/quote6.png" onClick={() => this.handleAddOverlay('/quote6.png')} width="100px" />
-            <img src="/quote7.png" onClick={() => this.handleAddOverlay('/quote7.png')} width="100px" />
-            <img src="/quote8.png" onClick={() => this.handleAddOverlay('/quote8.png')} width="100px" />
-            <img src="/quote9.png" onClick={() => this.handleAddOverlay('/quote9.png')} width="100px" />
-            <img src="/quote10.png" onClick={() => this.handleAddOverlay('/quote10.png')} width="100px" />
-            <img src="/quote11.png" onClick={() => this.handleAddOverlay('/quote11.png')} width="100px" />
+          <img
+            src="/captionbox.png"
+            onClick={() => this.handleAddOverlay("/captionbox.png")}
+            width="100px"
+          />
+          <img
+            src="/quote1.png"
+            onClick={() => this.handleAddOverlay("/quote1.png")}
+            width="100px"
+          />
+          <img
+            src="/quote2.png"
+            onClick={() => this.handleAddOverlay("/quote2.png")}
+            width="100px"
+          />
+          <img
+            src="/quote3.png"
+            onClick={() => this.handleAddOverlay("/quote3.png")}
+            width="100px"
+          />
+          <img
+            src="/quote4.png"
+            onClick={() => this.handleAddOverlay("/quote4.png")}
+            width="100px"
+          />
+          <img
+            src="/quote5.png"
+            onClick={() => this.handleAddOverlay("/quote5.png")}
+            width="100px"
+          />
+          <img
+            src="/quote6.png"
+            onClick={() => this.handleAddOverlay("/quote6.png")}
+            width="100px"
+          />
+          <img
+            src="/quote7.png"
+            onClick={() => this.handleAddOverlay("/quote7.png")}
+            width="100px"
+          />
+          <img
+            src="/quote8.png"
+            onClick={() => this.handleAddOverlay("/quote8.png")}
+            width="100px"
+          />
+          <img
+            src="/quote9.png"
+            onClick={() => this.handleAddOverlay("/quote9.png")}
+            width="100px"
+          />
+          <img
+            src="/quote10.png"
+            onClick={() => this.handleAddOverlay("/quote10.png")}
+            width="100px"
+          />
+          <img
+            src="/quote11.png"
+            onClick={() => this.handleAddOverlay("/quote11.png")}
+            width="100px"
+          />
         </div>
       </div>
     );
--- component/Canvas.jsx
+++ component/Canvas.jsx
@@ -1,22 +1,22 @@
-import React, { Component } from 'react';
-import { Layer, Stage, Image } from 'react-konva';
-import Konva from 'konva';
-import Photo from './Photo';
-import axios from 'axios';
-import { db } from '../fire';
-import { Redirect } from 'react-router';
+import React, { Component } from "react";
+import { Layer, Stage, Image } from "react-konva";
+import Konva from "konva";
+import Photo from "./Photo";
+import axios from "axios";
+import { db } from "../fire";
+import { Redirect } from "react-router";
 
 export default class Canvas extends Component {
   constructor(props) {
     super(props);
     this.state = {
       canvasImages: [],
-      canvasUrl: '',
+      canvasUrl: "",
       user: {},
-      storyId: '',
-      background: 'canvas-white',
+      storyId: "",
+      background: "canvas-white",
       fireRedirect: false
-    }
+    };
     this.uploadToCloudinary = this.uploadToCloudinary.bind(this);
     this.uploadFile = this.uploadFile.bind(this);
     this.changeBackgroundColor = this.changeBackgroundColor.bind(this);
@@ -31,7 +31,7 @@ export default class Canvas extends Component {
   }
 
   changeBackgroundColor(e) {
-    this.setState({background: e.target.value})
+    this.setState({ background: e.target.value });
   }
 
   uploadFile(dataUrl) {
@@ -40,17 +40,17 @@ export default class Canvas extends Component {
     const storyId = this.state.storyId;
     // const imageUrl = this.state.canvasUrl;
     //this function uploads image to cloudinary
-    const cloudName = 'noorulain';
-    const cloudPreset = 'pvfhdtk2';
+    const cloudName = "noorulain";
+    const cloudPreset = "pvfhdtk2";
     var url = `https://api.cloudinary.com/v1_1/${cloudName}/image/upload`;
     //construct a set of key/value pairs representing form fields and their values -- which can then be easily sent
     //with the request.
     const fd = new FormData();
-    fd.append('upload_preset', cloudPreset);
-    fd.append('file', dataUrl);
+    fd.append("upload_preset", cloudPreset);
+    fd.append("file", dataUrl);
     return axios
       .post(url, fd, {
-        headers: { 'X-Requested-With': 'XMLHttpRequest' }
+        headers: { "X-Requested-With": "XMLHttpRequest" }
       })
       .then(response =>
         this.setState({
@@ -59,31 +59,30 @@ export default class Canvas extends Component {
       )
       .then(() =>
         db
-          .collection('scenes')
+          .collection("scenes")
           .doc(key)
           .set({ imageUrl: this.state.canvasUrl })
       )
       .then(() =>
         db
-          .collection('stories')
+          .collection("stories")
           .doc(storyId)
-          .collection('scenes')
+          .collection("scenes")
           .doc(key)
-          .set({ imageUrl: this.state.canvasUrl, id: key, random: 'check' })
+          .set({ imageUrl: this.state.canvasUrl, id: key, random: "check" })
       )
       .then(() =>
         db
-          .collection('stories')
+          .collection("stories")
           .doc(storyId)
           .update({ thumbnail: this.state.canvasUrl })
       )
       .then(() => this.setState({ fireRedirect: true }))
-      .catch(error => console.error('Error creating scene: ', error));
+      .catch(error => console.error("Error creating scene: ", error));
   }
 
   uploadToCloudinary() {
-    const image = this.stageRef.getStage().toDataURL('image/png');
-    console.log(image);
+    const image = this.stageRef.getStage().toDataURL("image/png");
     this.uploadFile(image);
   }
 
@@ -105,16 +104,21 @@ export default class Canvas extends Component {
                 const image = new window.Image();
                 image.crossOrigin = "Anonymous"; //causing async issues. //must use otherwise tainted canvas error.
                 image.src = imageUrl;
-                return (<Photo
-                  key={`${imageUrl}${index}`}
-                  imageUrl={imageUrl}
-                  image={image}
-                  zindex={index}
-                   />)
-            })}
+                return (
+                  <Photo
+                    key={`${imageUrl}${index}`}
+                    imageUrl={imageUrl}
+                    image={image}
+                    zindex={index}
+                  />
+                );
+              })}
           </Layer>
         </Stage>
-        <select defaultValue="canvas-white" onChange={this.changeBackgroundColor}>
+        <select
+          defaultValue="canvas-white"
+          onChange={this.changeBackgroundColor}
+        >
           <option value="canvas-white">White</option>
           <option value="canvas-pinkdots">Pink Dots</option>
           <option value="canvas-bluedots">Blue Dots</option>
