574bc34f28bda1398723d2563a2d39183df5441d
Jeanne Castillo <jcaron27@gmail.com>
2017-11-29 13:42:35 -0500

    fix(collab & stories) collab saves and stories load

--- component/Stories.jsx
+++ component/Stories.jsx
@@ -11,72 +11,53 @@ export default class Stories extends Component {
       scenes: [],
       userId: ''
     };
-
-    this.setStateToStories = this.setStateToStories.bind(this);
   }
 
   componentWillReceiveProps(nextProps) {
     let self = this;
 
-    let getCurrentUser = function() {
-      return new Promise((resolve, reject) => {
-        console.log("getCurrentUser", nextProps.currentUser)
-        return resolve(nextProps.currentUser.uid)
-      })
-    }
-
-    let getUserStories = function(id) {
-      return new Promise((resolve, reject) => {
-        console.log("getUserStories", id)
-        let stories = []
-        return resolve(db.collection('users').doc(id).collection('stories')
-        .get()
-        .then((snapshot) => {
-          snapshot.forEach(doc => {
-            stories.push(doc.data())
-          })
-          return stories;
-        }))
-      })
-    }
-
-    let getStories = function(stories) {
-      return new Promise((resolve, reject) => {
-        console.log("getStories", stories)
-        let filteredStories = stories.filter(story => story.id)
-        console.log("filteredStories", filteredStories)
-        let userStories = [];
-        filteredStories.map(story => {
-          db.collection('stories').where('id', '==', story.id)
+    if (nextProps.currentUser && nextProps.currentUser !== {} ) {
+      let getCurrentUser = function() {
+        return new Promise((resolve, reject) => {
+          return resolve(nextProps.currentUser.uid)
+        })
+      }
+  
+      let getUserStories = function(id) {
+        return new Promise((resolve, reject) => {
+          let stories = []
+          return resolve(db.collection('users').doc(id).collection('stories')
           .get()
           .then((snapshot) => {
             snapshot.forEach(doc => {
-              userStories.push(doc.data())
+              stories.push(doc.data())
+            })
+            return stories;
+          }))
+        })
+      }
+  
+      let getStories = function(stories) {
+        return new Promise((resolve, reject) => {
+          let filteredStories = stories.filter(story => story.id)
+          filteredStories.map(story => {
+            db.collection('stories').where('id', '==', story.id)
+            .get()
+            .then((snapshot) => {
+              snapshot.forEach(doc => {
+                self.setState({stories: [...self.state.stories, doc.data()]})
+              })
             })
           })
         })
-        return resolve(userStories)
-      })
-    }
+      }
 
-    if(self.state.isMounted) {
       getCurrentUser().then(function(id) {
         return getUserStories(id)
       })
       .then(function(stories) {
         return getStories(stories)
       })
-      .then(function(stories) {
-        return self.setStateToStories(stories)
-      })
-    }
-    
-  }
-
-  setStateToStories(stories) {
-    console.log("stories last", stories)
-    if(this.refs.myRef) {
-      this.setState({stories})
     }
   }
 
@@ -89,10 +70,9 @@ export default class Stories extends Component {
       <div className="stories">
         <h1 className="stories-title">Your Stories</h1>
         <div className="stories-container">
-          {this.state.stories.map(story => {
-            console.log("story", story);
-            let id = story.data().id;
-            let thumbnail = story.data().thumbnail || "/default.png"; //default image if no scenes exist
+          {this.state.stories.length ? this.state.stories.map(story => {
+            let id = story.id;
+            let thumbnail = story.thumbnail || "/default.png"; //default image if no scenes exist
 
             return (
                 <div className="stories-story" key={id}>
@@ -102,14 +82,14 @@ export default class Stories extends Component {
                   <div className="stories-story-content">
                     <h2 className="stories-story-content-title">
                       <a className="stories-story-content-title" href={`/stories/${id}`}>
-                        {story.data().title}
+                        {story.title}
                       </a>
                     </h2>
-                    <p className="stories-story-content-description">{story.data().description}</p>
+                    <p className="stories-story-content-description">{story.description}</p>
                   </div>
                 </div>
             );
-          })}
+          }): <h1 className="stories-title">Loading...</h1>}
         </div>
       </div>
     )
