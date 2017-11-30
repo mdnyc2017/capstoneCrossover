7ad1d682b6cb054ac64f0cf5b0332e0cd5e07034
mcabrex <mrcabardo@gmail.com>
2017-11-30 14:43:55 -0500

    temporary collab edit

--- component/SingleStory.jsx
+++ component/SingleStory.jsx
@@ -1,132 +1,125 @@
-import React, { Component } from "react";
-import { db } from "~/fire";
+import React, { Component } from 'react';
+import { db } from '~/fire';
 
 export default class SingleStory extends Component {
   constructor(props) {
     super(props);
-    //state is composed of current user, current story, and story's scenes
+    // state is composed of current user, current story, and story's scenes
     this.state = {
       user: {},
-      storyId: "",
-      storyTitle: "",
-      storyDescription: "",
+      storyId: '',
+      storyTitle: '',
+      storyDescription: '',
       scenes: [],
       collaborator: {},
-      collaboratorName: []
+      collaboratorName: [],
     };
   }
 
   componentDidMount() {
-    //Get Scenes
+    // Get Scenes
     db
-      .collection("stories")
+      .collection('stories')
       .doc(this.props.match.params.id)
-      .collection("scenes")
+      .collection('scenes')
       .onSnapshot(snapshot =>
         this.setState({
-          scenes: snapshot.docs
-        })
-      );
+          scenes: snapshot.docs,
+        }));
 
-    //Get Story info
+    // Get Story info
     db
-      .collection("stories")
+      .collection('stories')
       .doc(this.props.match.params.id)
       .onSnapshot(snapshot =>
         this.setState({
           storyTitle: snapshot.data().title,
-          storyDescription: snapshot.data().description
-        })
-      );
+          storyDescription: snapshot.data().description,
+        }));
 
-      // //get collaborator
-      // db
-      // .collection('collaborators')
-      // .where('storyId', '==', this.props.match.params.id)
-      // .onSnapshot( snapshot =>{
-      //   this.setState({
-      //     collaboratorName: snapshot.data().collabName
-      //   })
-      // })
+    // //get collaborator
+    // db
+    // .collection('collaborators')
+    // .where('storyId', '==', this.props.match.params.id)
+    // .onSnapshot( snapshot =>{
+    //   this.setState({
+    //     collaboratorName: snapshot.data().collabName
+    //   })
+    // })
   }
 
   componentWillReceiveProps(nextProps) {
-    //We set the user on state to the current user & the storyId to the id in the URL path
+    // We set the user on state to the current user & the storyId to the id in the URL path
     this.setState({
       user: nextProps.currentUser,
-      storyId: this.props.match.params.id
+      storyId: this.props.match.params.id,
     });
 
-    //Get Scenes
+    // Get Scenes
     db
-      .collection("stories")
+      .collection('stories')
       .doc(this.props.match.params.id)
-      .collection("scenes")
+      .collection('scenes')
       .onSnapshot(snapshot =>
         this.setState({
-          scenes: snapshot.docs
-        })
-      );
+          scenes: snapshot.docs,
+        }));
 
-    //Get Story info
+    // Get Story info
     db
-      .collection("stories")
+      .collection('stories')
       .doc(this.props.match.params.id)
       .onSnapshot(snapshot =>
         this.setState({
           storyTitle: snapshot.data().title,
-          storyDescription: snapshot.data().description
-        })
-      );
+          storyDescription: snapshot.data().description,
+        }));
 
 
-      //get collaborator
-      db
+    // get collaborator
+    db
       .collection('stories')
       .doc(this.props.match.params.id)
       .collection('collaborators')
-      .onSnapshot(snapshot =>{
-        console.log('snapshot in collab call is: ', snapshot)
+      .onSnapshot((snapshot) => {
+        console.log('snapshot in collab call is: ', snapshot);
         this.setState({
-          collaboratorName: snapshot.docs
-        })
-      })
+          collaboratorName: snapshot.docs,
+        });
+      });
 
 
-
-
-
-      // .where('storyId', '==', this.props.match.params.id)
-      // .onSnapshot( snapshot =>{
-      //   console.log('in single story collab call, snapshot is:', snapshot)
-      //   this.setState({
-      //     collaboratorName: snapshot.data().collabName
-      //   })
-      // })
+    // .where('storyId', '==', this.props.match.params.id)
+    // .onSnapshot( snapshot =>{
+    //   console.log('in single story collab call, snapshot is:', snapshot)
+    //   this.setState({
+    //     collaboratorName: snapshot.data().collabName
+    //   })
+    // })
   }
 
   render() {
-
->>>>>>> origin/master
     return (
       <div className="single-story">
 
         <h4 className="single-story-title">{this.state.storyTitle}</h4>
         <p className="single-story-description">{this.state.storyDescription && this.state.storyDescription}</p>
-        
+
         {this.state.collaboratorName.length ?
-        <div className="single-story-collaborating">
-          collaborating with{
-            this.state.collaboratorName.map(collaborator=>(
-              <div key={collaborator+Date.now()}>
-               <li>{collaborator.data().collabName}</li>
+          <div className="single-story-collaborating">
+            <div className="single-story-collaborating-title" >Featuring :</div>
+            {
+            this.state.collaboratorName.map(collaborator => (
+              <div key={collaborator + Date.now()}>
+                <div>{collaborator.data().collabName}</div>
               </div>
 
-            ))}
-        </div>        
+            ))
+          }
+          </div>
         :
-        <span> </span>    
-        } 
+          <span />
+        }
 
 
         <div className="single-story-scenes">
@@ -136,7 +129,6 @@ export default class SingleStory extends Component {
             </div>
           ))}
         </div>
-
 
 
         <a className="single-story-add-link" href={`/stories/${this.props.match.params.id}/addscene`}>
